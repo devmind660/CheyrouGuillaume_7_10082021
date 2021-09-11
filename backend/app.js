ecf3745b3c04569365ba1fb936a1c10bc4f719d4
Guillaume Cheyrou <devmind660@gmail.com>
2021-09-11 08:30:19 +0200

    Ajout de la logique métier backend.

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+// Dépendances
+const express = require('express');
+const helmet = require("helmet");
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Routes
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+
+// Framework Express
+const app = express();
+
+// Configuration du header des requêtes pour la communication cross-serveur (empêche l\'erreur CORS)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Module de sécurité helmet
+app.use(helmet());
+
+// Transforme le corps des requêtes en objet JSON
+app.use(bodyParser.json());
+
+// Accès au dossier gif
+app.use('/gif', express.static(path.join(__dirname, 'images')));
+
+// Routes
+app.use('/api/posts', postsRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/models/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+// Mise en place de la base de donnée
+let connection = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.DBPORT,
+    user: process.env.DBUSER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+connection.connect(function(error) {
+    if (error) throw error;
+    console.log('You are now connected to MySQL...')
+});
+
+module.exports = connection;
