9e6cc8c88c4d33f8693b2b6ddf339788e42260ae
Guillaume Cheyrou <devmind660@gmail.com>
2021-12-28 21:32:35 +0100

    Rebuild de la BDD

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const postCtrl = require('../controllers/comments');
 
 router.post('/', postCtrl.createComment);
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const postCtrl = require('../controllers/posts');
 
 router.get('/', postCtrl.showFeed);
